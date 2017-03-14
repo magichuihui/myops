d5b8cd1db72e477257c7a1753f3e8f1c9bcb809f
kyra <magichuihui@gmail.com>
2017-03-14 10:04:57 +0800

    add mysql backup strategy

--- MySQL/backup.sh
+++ MySQL/backup.sh
@@ -6,35 +6,47 @@ mysql_dir=/var/mysql/data
 backup_dir=/mydata/mysql_backup/backup
 binlog=mysql-bin
 host=192.168.0.226
-day_of_week=$(date +"%w")
-week_of_year=$(date +"%U")
+declare -i day_of_week=$(date +"%w")
+declare -i week_of_year=$(date +"%U")
 
 # enter mysql data directory
 cd $mysql_dir
 
-mkdir $backup_dir/$week_of_year
+mkdir -p $backup_dir/$week_of_year
 
-# Copy binlog file 
-mysqladmin -ubackup -ppassword -h$host flush-logs binary
-cp `tac $binlog.index | sed -n '2p'` $backup_dir/$week_of_year
+# On Weekday or Saturday, flush binlog
+if [ $day_of_week -gt 0 ]; then
+
+    # Flush & copy binlog file 
+    mysqladmin -ubackup -ppassword -h$host flush-logs binary
+    cp `tac $binlog.index | sed -n '2p'` $backup_dir/$week_of_year
+
+fi
 
 # when Sunday, make an full backup
 if [ $day_of_week -eq 0 ]; then
 
-    # delete binlogs except the last one
-    rm -f `sed -n '$!p' $mysql_dir/$binlog.index`
+    # If it's the first Sunday in a year, move the binlog file to parent directory
+    if [ $week_of_year -eq 0 ]; then
+        last_backup_dir=""
+    else
+        declare -i last_backup_dir=$week_of_year-1
+    fi
+
+    mkdir -p $backup_dir/$last_backup_dir
 
     # backup db
-    mysqldump -ubackup -ppassword -h$host --all-databases --single-transaction --quick > $backup_dir/$week_of_year/`date +"%Y%m%d"`.sql
+    mysqldump -ubackup -ppassword -h$host --all-databases --single-transaction --flush-logs --quick --master-data=2 --delete-master-logs > /tmp/mysql_backup.sql
+    mv /tmp/mysql_backup.sql $backup_dir/$week_of_year/`date +"%Y%m%d"`.sql
 
-    # delete the last binlog
-    #rm -f `sed -n '$p' $mysql_dir/$binlog.index`
+    # Copy the last week's binlog
+    cp `tac $binlog.index | sed -n '2p'` $backup_dir/$last_backup_dir
 
-    # create new binlog by flush logs
-    #mysqladmin -ubackup -ppassword -h$host flush-logs binary
+    # delete binlogs except the last one
+    #rm -f `sed -n '$!p' $mysql_dir/$binlog.index`
 
     # write new binlog info to mysql-bin.inex
-    sed -i '$ !d' $mysql_dir/mysql-bin.index
+    #sed -i '$ !d' $mysql_dir/mysql-bin.index
 
 fi
 
